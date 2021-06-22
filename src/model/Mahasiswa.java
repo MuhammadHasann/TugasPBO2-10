3816d72121968da28366e5c32d9c6545d4f2f61d
MuhammadHasann <muhammadhas772@gmail.com>
2021-06-22 22:10:14 +0700

    no.6 membuat getter dan setter di class 1

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -42,16 +42,83 @@ public class Mahasiswa {
         System.out.println("ini dari constructor Mahasiswa");
     }
 
+    //ini contructor pertama
     public Mahasiswa(String namaMahasiswa) {
         this.namaMahasiswa = namaMahasiswa;
     }
 
+    //ini contructor kedua
     public Mahasiswa(int jumlahSks) {
         this.jumlahSks = jumlahSks;
     }
 
+    //ini contructor ketiga
     public Mahasiswa(int telepon, String alamat) {
         this.telepon = telepon;
+        this.alamat = alamat;
+    }
+
+    public String getNamaMahasiswa() {
+        return namaMahasiswa;
+    }
+
+    public void setNamaMahasiswa(String namaMahasiswa) {
+        this.namaMahasiswa = namaMahasiswa;
+    }
+
+    public int getNpm() {
+        return npm;
+    }
+
+    public void setNpm(int npm) {
+        this.npm = npm;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    public int getJumlahSks() {
+        return jumlahSks;
+    }
+
+    public void setJumlahSks(int jumlahSks) {
+        this.jumlahSks = jumlahSks;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    public int getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(int telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
 }

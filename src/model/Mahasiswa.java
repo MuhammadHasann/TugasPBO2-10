fba05c520394dc7fa8d7e056586bb4951f806cc4
MuhammadHasann <muhammadhas772@gmail.com>
2021-06-22 19:30:02 +0700

    no.1 membuat class pertama (8 atribut, 4 method)

---
+++ src/model/Mahasiswa.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Mahasiswa {
+    public String namaMahasiswa;
+    public int npm;
+    public String kelas;
+    public String prodi;
+    public int jumlahSks;
+    public double ipk;
+    public int telepon;
+    public String alamat;
+
+
+
+    public void tampilkanIdentitas(){
+        System.out.println("Nama = " + namaMahasiswa);
+        System.out.println("NPM = " + npm);
+        System.out.println("Telepon = " + telepon);
+        System.out.println("Alamat = " + alamat);
+
+    }
+    public void tampilkanProdi(){
+        System.out.println("Kelas = " + kelas);
+        System.out.println("Prodi = " + prodi);
+
+    }
+    public void tampilkanSks(){
+        System.out.println("Jumlah SKS = " + jumlahSks);
+
+    }
+    public void tampilkanIpk(){
+        System.out.println("IPK = " + ipk);
+
+    }
+}
